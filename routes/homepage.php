db10c310cbb4617e1afcc8594f0eab366a09822d
Nafiur Rahman <nrsajib@gmail.com>
2024-07-04 20:03:00 +0600

    HomePage

--- routes/homepage.php
+++ routes/homepage.php
@@ -1,8 +1,11 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\HomePageController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [HomePageController::class, 'index'])->name('homepage.index');
